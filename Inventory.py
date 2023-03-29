471bd285becbf35d649732ed7bcc0dcda242cecd
Festo Wasukira <126950066+EthEl2524@users.noreply.github.com>
2023-03-29 21:38:06 +0300

    Add files via upload

---
+++ Inventory.py
@@ -0,0 +1,54 @@
+import openpyxl
+
+inventory_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inventory_file["Sheet1"]
+
+
+#  creating dictionaries
+product_per_supplier = {}
+total_value_per_supplier = {}
+products_under_10_inv = {}
+products_over_100_inv = {}
+
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_num = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
+
+
+    # calculation for number of products per supplier
+    if supplier_name in product_per_supplier:
+        current_no_Products = product_per_supplier.get(supplier_name)
+        product_per_supplier[supplier_name] = current_no_Products + 1
+    else:
+        product_per_supplier[supplier_name] = 1
+
+    # total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
+    # logic products with inventory less than 10
+    if inventory < 10:
+        products_under_10_inv[int(product_num)] = int(inventory)
+
+    # logic products with inventory greater than 100
+    if inventory < 100:
+        products_over_100_inv[int(product_num)] = int(inventory)
+
+    # add value for total inventory price
+    inventory_price.value = inventory * price
+
+print(product_per_supplier)
+print(total_value_per_supplier)
+print(products_under_10_inv)
+print(products_over_100_inv)
+print(inventory_price.value)
+
+
+inventory_file.save("inventory_with_total_value.xlsx")
